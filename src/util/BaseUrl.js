dd8fc320464831b3d2b3a89c57184f40ffb87b21
Harshit Patel <harshit3228@gmail.com>
2022-11-26 10:21:05 +0530

    add api url

--- src/util/BaseUrl.js
+++ src/util/BaseUrl.js
@@ -1,2 +1,4 @@
-// eslint-disable-next-line import/prefer-default-export
-export const baseURL = 'http://localhost:80/api';
+/* eslint-disable import/prefer-default-export */
+
+// export const baseURL = 'http://localhost:80/api';
+export const baseURL = 'https://capitaldeck.onrender.com/api';

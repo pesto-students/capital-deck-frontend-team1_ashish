120983f783905fc73f313dcbf1904d00398c26d5
Harshit Patel <harshit3228@gmail.com>
2022-12-09 22:37:27 +0530

    end point change

--- src/util/BaseUrl.js
+++ src/util/BaseUrl.js
@@ -3,5 +3,5 @@
 // export const baseURL = 'http://localhost:80/api';
 // export const baseUploadURL = 'http://localhost:80/';
 
-export const baseURL = 'https://capitaldeck.onrender.com/api';
-export const baseUploadURL = 'https://capitaldeck.onrender.com/';
+export const baseURL = 'https://capital-deck.onrender.com/api';
+export const baseUploadURL = 'https://capital-deck.onrender.com/';

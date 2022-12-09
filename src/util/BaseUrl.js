1b2306f293c0b9b27f8de01fc00da8f4229bc11a
Salman Zafar <18salmanz@gmail.com>
2022-12-09 11:04:05 +0530

    Update BaseUrl.js

--- src/util/BaseUrl.js
+++ src/util/BaseUrl.js
@@ -3,5 +3,8 @@
 export const baseURL = 'http://localhost:80/api';
 export const baseUploadURL = 'http://localhost:80/';
 
+export const baseURL = 'http://ec2-52-66-197-131.ap-south-1.compute.amazonaws.com:5001/api';
+export const baseUploadURL = 'http://ec2-52-66-197-131.ap-south-1.compute.amazonaws.com:5001/';
+
 // export const baseURL = 'https://capitaldeck.onrender.com/api';
 // export const baseUploadURL = 'https://capitaldeck.onrender.com/';

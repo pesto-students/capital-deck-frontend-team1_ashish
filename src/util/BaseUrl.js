c9029de0e6dd212c65167bcd53224779098ad9c0
Salman Zafar <18salmanz@gmail.com>
2022-12-09 11:04:19 +0530

    Update BaseUrl.js

--- src/util/BaseUrl.js
+++ src/util/BaseUrl.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 
-export const baseURL = 'http://localhost:80/api';
-export const baseUploadURL = 'http://localhost:80/';
+// export const baseURL = 'http://localhost:80/api';
+// export const baseUploadURL = 'http://localhost:80/';
 
 export const baseURL = 'http://ec2-52-66-197-131.ap-south-1.compute.amazonaws.com:5001/api';
 export const baseUploadURL = 'http://ec2-52-66-197-131.ap-south-1.compute.amazonaws.com:5001/';

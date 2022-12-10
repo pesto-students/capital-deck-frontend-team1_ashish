78d93f194b71e7a20c52f111144a37fcc17ff260
Harshit Patel <harshit3228@gmail.com>
2022-12-10 17:42:43 +0530

    capital alert changes

--- src/util/BaseUrl.js
+++ src/util/BaseUrl.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 
-export const baseURL = 'http://localhost:80/api';
-export const baseUploadURL = 'http://localhost:80/';
+// export const baseURL = 'http://localhost:80/api';
+// export const baseUploadURL = 'http://localhost:80/';
 
-// export const baseURL = 'https://capital-deck.onrender.com/api';
-// export const baseUploadURL = 'https://capital-deck.onrender.com/';
+export const baseURL = 'https://capital-deck.onrender.com/api';
+export const baseUploadURL = 'https://capital-deck.onrender.com/';

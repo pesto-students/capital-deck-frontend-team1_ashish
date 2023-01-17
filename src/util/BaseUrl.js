4e5d4cf0ce3e40770c5f5ce6ee7bb80194b17a1e
Salman Zafar <18salmanz@gmail.com>
2023-01-17 22:44:16 +0530

    add url for vercel deployment

--- src/util/BaseUrl.js
+++ src/util/BaseUrl.js
@@ -1,5 +1,10 @@
 // export const baseURL = 'http://localhost:80/api';
 // export const baseUploadURL = 'http://localhost:80/';
 
-export const baseURL = 'https://capital-deck.onrender.com/api';
-export const baseUploadURL = 'https://capital-deck.onrender.com/';
+//! For Render Deployment
+// export const baseURL = 'https://capital-deck.onrender.com/api';
+// export const baseUploadURL = 'https://capital-deck.onrender.com/';
+
+//! For Vercel Deployment
+export const baseURL = 'https://capital-deck-backend.vercel.app/api';
+export const baseUploadURL = 'https://capital-deck-backend.vercel.app/';

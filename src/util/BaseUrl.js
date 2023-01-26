32c544a5aaca3b723671a9b84b7a5ecfbb7ac01f
Salman Zafar <18salmanz@gmail.com>
2023-01-26 11:39:43 +0530

    change deployment url to render

--- src/util/BaseUrl.js
+++ src/util/BaseUrl.js
@@ -2,9 +2,9 @@
 // export const baseUploadURL = 'http://localhost:80/';
 
 //! For Render Deployment
-// export const baseURL = 'https://capital-deck.onrender.com/api';
-// export const baseUploadURL = 'https://capital-deck.onrender.com/';
+export const baseURL = 'https://capital-deck.onrender.com/api';
+export const baseUploadURL = 'https://capital-deck.onrender.com/';
 
-//! For Vercel Deployment
-export const baseURL = 'https://capital-deck-backend.vercel.app/api';
-export const baseUploadURL = 'https://capital-deck-backend.vercel.app/';
+// //! For Vercel Deployment
+// export const baseURL = 'https://capital-deck-backend.vercel.app/api';
+// export const baseUploadURL = 'https://capital-deck-backend.vercel.app/';

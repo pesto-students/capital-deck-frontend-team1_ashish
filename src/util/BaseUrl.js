51d56fdb699152dd6299b6f9e74e019383b55794
Salman Zafar <18salmanz@gmail.com>
2023-01-26 12:50:48 +0530

    add deployment url for railway

--- src/util/BaseUrl.js
+++ src/util/BaseUrl.js
@@ -2,9 +2,13 @@
 // export const baseUploadURL = 'http://localhost:80/';
 
 //! For Render Deployment
-export const baseURL = 'https://capital-deck.onrender.com/api';
-export const baseUploadURL = 'https://capital-deck.onrender.com/';
+// export const baseURL = 'https://capital-deck.onrender.com/api';
+// export const baseUploadURL = 'https://capital-deck.onrender.com/';
 
 // //! For Vercel Deployment
 // export const baseURL = 'https://capital-deck-backend.vercel.app/api';
 // export const baseUploadURL = 'https://capital-deck-backend.vercel.app/';
+
+// //! For Railway Deployment
+export const baseURL = 'https://capital-deck-backend-production.up.railway.app/api';
+export const baseUploadURL = 'https://capital-deck-backend-production.up.railway.app/';

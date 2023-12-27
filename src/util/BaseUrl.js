357a113eaa52a630421b3fefb9dfb0ac4786a318
HarshitPatel32 <121165660+HarshitPatel32@users.noreply.github.com>
2023-12-28 02:29:38 +0530

    backend url change

--- src/util/BaseUrl.js
+++ src/util/BaseUrl.js
@@ -2,13 +2,13 @@
 // export const baseUploadURL = 'http://localhost:80/';
 
 //! For Render Deployment
-// export const baseURL = 'https://capital-deck.onrender.com/api';
-// export const baseUploadURL = 'https://capital-deck.onrender.com/';
+export const baseURL = 'https://capitaldeck.onrender.com/api';
+export const baseUploadURL = 'https://capitaldeck.onrender.com/';
 
 // //! For Vercel Deployment
 // export const baseURL = 'https://capital-deck-backend.vercel.app/api';
 // export const baseUploadURL = 'https://capital-deck-backend.vercel.app/';
 
 // //! For Railway Deployment
-export const baseURL = 'https://capital-deck-backend-production.up.railway.app/api';
-export const baseUploadURL = 'https://capital-deck-backend-production.up.railway.app/';
+// export const baseURL = 'https://capital-deck-backend-production.up.railway.app/api';
+// export const baseUploadURL = 'https://capital-deck-backend-production.up.railway.app/';
